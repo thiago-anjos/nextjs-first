62215a723855e52cd3532863c0120fb1c37cde1b
thiagoanjos <thiago.w3c@gmail.com>
2021-03-07 21:17:46 -0300

    styled-components-static render

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../styles/global-styles';
+import { theme } from '../styles/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
