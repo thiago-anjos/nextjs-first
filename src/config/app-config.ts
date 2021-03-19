43bb95376108d7f2096a6ed61bcba0176642cdbb
thiagoanjos <thiago.w3c@gmail.com>
2021-03-18 23:13:21 -0300

    fix(change name): change name

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,4 @@
 export const API_URL = 'https://secure-oasis-88915.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
-export const SITE_NAME = 'Thiago dos Anjos';
+export const SITE_NAME = 'Thiago dos Anjos Lopes';
 export const SITE_URL = 'https://keen-sammet-dd6013.netlify.app';
