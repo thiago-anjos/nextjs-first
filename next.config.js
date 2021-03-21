81984323e675bcb34a8fbaa23d7440a1d8ecc0be
thiagoanjos <thiago.w3c@gmail.com>
2021-03-21 02:43:42 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   target: 'serverless',
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-    return config;
-  },
 };
