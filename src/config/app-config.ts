f87680b30ecf187f73796b1e1ae6b25e6bed51c8
thiagoanjos <thiago.w3c@gmail.com>
2021-03-21 00:00:48 -0300

    test

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,4 @@
 export const API_URL = 'https://secure-oasis-88915.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 export const SITE_NAME = 'Thiago dos Anjos Lopes';
-export const SITE_URL = 'https://practical-khorana-5e72c0.netlify.app/';
+export const SITE_URL = 'https://practical-khorana-5e72c0.netlify.app';
